710698caed4e6391a0ab8eb4f2d54038e2eb3707
Natallia Behlik <natasha-1909@yandex.ru>
2020-03-22 23:16:49 +0300

    feat: add solution count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members) || members.length === 0) {
+    return false;
+  }
+let arr = members.filter(el => typeof el === 'string');
+
+if(!arr || arr.length === 0) {
+  return false;
+}
+
+return arr.map(el => el.trim()).map(el => el.toUpperCase()).sort().map(el => el[0]).join('');
+
 };
